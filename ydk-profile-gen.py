ef16926cfd95450a7e3cbb485153b2dfe98fbb77
Taran Deshpande <tadeshpa@cisco.com>
2020-06-17 10:04:23 -0400

    Use different templates for all os's

--- ydk-profile-gen.py
+++ ydk-profile-gen.py
@@ -10,6 +10,7 @@ from git import Repo
 parser = argparse.ArgumentParser()
 
 parser.add_argument("path")
+parser.add_argument('--os-name', required=True, choices=['Cisco-IOS-XR', 'Cisco-IOS-XE', 'Cisco-NX-OS'])
 parser.add_argument('--version', required=True)
 parser.add_argument('--core-version', required=True)
 parser.add_argument('--dependency-name', required=True)
@@ -20,6 +21,11 @@ parser.add_argument('--dependency-uri', required=True)
 args = parser.parse_args()
 
 profile_path = args.path
+
+if profile_path[-1] is '/':
+    profile_path = profile_path[:-1]
+
+os_name = args.os_name
 
 version = args.version
 
@@ -41,17 +47,17 @@ remote_repo_url = repo.remotes.origin.url
 commit_id = repo.commit('HEAD').hexsha
 dir_files = ls(profile_path).split()
 
-cisco_ios_xr_files = []
-tail_f_files = []
+os_files = []
+tailf_files = []
 other_files = []
 
 for dir_file in dir_files:
     if (fnmatch.fnmatch(dir_file, '*deviations.yang')) or ('openconfig' in dir_file) or ('ietf' in dir_file) or ('iana' in dir_file):
         continue
-    if fnmatch.fnmatch(dir_file, 'Cisco-IOS-XR*.yang'):
-        cisco_ios_xr_files.append(profile_path + '/' + dir_file)
-    elif fnmatch.fnmatch(dir_file, 'tail_f*.yang'):
-        tail_f_files.append(profile_path + '/' + dir_file)
+    elif fnmatch.fnmatch(dir_file, f'{os_name}*.yang'):
+        os_files.append(profile_path + '/' + dir_file)
+    elif fnmatch.fnmatch(dir_file, 'tailf*.yang'):
+        tailf_files.append(profile_path + '/' + dir_file)
     elif fnmatch.fnmatch(dir_file, '*.yang'):
         other_files.append(profile_path + '/' + dir_file)
 
@@ -64,30 +70,31 @@ def convert_to_json_file_list(file_list):
     else:
         return ''
 
-cisco_ios_xr_files = convert_to_json_file_list(cisco_ios_xr_files)
-if cisco_ios_xr_files and (tail_f_files or other_files):
-    cisco_ios_xr_files += ','
+os_files = convert_to_json_file_list(os_files)
+if os_files and (tailf_files or other_files):
+    os_files += ',' + '\n\n                '
 
-tail_f_files = convert_to_json_file_list(tail_f_files)
-if tail_f_files and other_files:
-    tail_f_files += ','
+tailf_files = convert_to_json_file_list(tailf_files)
+if tailf_files and other_files:
+    tailf_files += ',' + '\n\n                '
+
 
 other_files = convert_to_json_file_list(other_files)
+
+yang_files = os_files + tailf_files + other_files
 
 template_vars = {
     'version': version,
     'core_version': core_version,
     'url': remote_repo_url,
     'commit_id': commit_id,
-    'cisco_ios_xr_files': cisco_ios_xr_files,
-    'tail_f_files': tail_f_files,
-    'other_files': other_files,
+    'yang_files': yang_files,
     'dependency_name': dependency_name,
     'dependency_version': dependency_version,
     'dependency_core_version': dependency_core_version,
     'dependency_uri': dependency_uri
 }
 
-with open('cisco-ios-xr_template.json', 'r') as template:
+with open(f'{os_name}_template.json', 'r') as template:
     template_string = template.read()
     print(template_string.format(**template_vars))
